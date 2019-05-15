6378bc0f731522620d7d53f561f26e48ed12612f
supermanc88 <supermanc88@gmail.com>
2019-05-15 18:06:47 +0800

    添加代码

---
+++ LeetCode/code/14. 最长公共前缀/test/test/test.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char * longestCommonPrefix(char ** strs, int strsSize)
+{
+	/*
+	 * 思路：
+	 * 先比较第一和第二个，如果有公共的前缀，则用前缀和第三个比较
+	 * 如果没有公共前缀，就直接返回，后面的不需要比较
+	 */
+
+}
+
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
