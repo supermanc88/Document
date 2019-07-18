8c55363bdf49003ef3918880231ce004e70a41e3
supermanc88 <supermanc88@gmail.com>
2019-07-18 14:10:12 +0800

    添加windows 网络编程例子

---
+++ TCPIP/Windows网络编程/Client/Client/Client.cpp
@@ -0,0 +1,60 @@
+﻿// Client.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+
+	WSADATA	wsaData;
+	// 初始化
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	// 创建socket套接字	socket流 TCP协议
+	SOCKET	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	// 设置请求目标
+	sockaddr_in sockAddr;
+	memset(&sockAddr, 0, sizeof(sockAddr));
+	sockAddr.sin_family = PF_INET;	//使用IPv4
+	sockAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");	//将字符串地址转换成数值
+	sockAddr.sin_port = htons(1234);
+
+	// 开始连接
+	connect(sock, (SOCKADDR *)&sockAddr, sizeof(SOCKADDR));
+
+	// 接收服务器传回的数据
+	char szBuffer[1024] = { 0 };
+	recv(sock, szBuffer, 1023, NULL);
+
+	// 打印接收到的数据
+	std::cout << "message form server:" << szBuffer << std::endl;
+
+	// 发送消息
+	char str[] = "this message come from Client";
+	send(sock, str, strlen(str) + 1, 0);
+
+	// 关闭套接字
+	closesocket(sock);
+
+	// 清理
+	WSACleanup();
+
+    std::cout << "Hello World!\n"; 
+}
+
+/*
+ * 客户端流程：
+ * 1. 初始化
+ * 2. 创建套接字
+ * 3. 设置地址信息
+ * 4. connet 连接服务端
+ * 5. recive 接收服务端发来的数据
+ * 6. send 向服务端发送数据
+ * 7. 关闭套接字
+ * 8. 清理
+ */
---
+++ TCPIP/Windows网络编程/Server/Server/Server.cpp
@@ -0,0 +1,75 @@
+﻿// Server.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+	//初始化
+	WSADATA wsaData;
+
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	// 创建套接字
+	SOCKET serverSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	// 设置地址信息
+	sockaddr_in sockAddr;
+	sockAddr.sin_family = PF_INET;
+	sockAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	sockAddr.sin_port = htons(1234);
+
+	// 绑定套接字
+	bind(serverSock, (SOCKADDR *)&sockAddr, sizeof(SOCKADDR));
+
+	// 开始监听
+	listen(serverSock, 20);
+
+	SOCKADDR clientAddr;
+	int nSize = sizeof(SOCKADDR);
+	while(1)
+	{
+		// 接收客户端请求，生成套接字
+		SOCKET clientSock = accept(serverSock, &clientAddr, &nSize);
+
+		char str[] = "hello Client, this message come from server";
+
+		// 向客户端发送数据
+		send(clientSock, str, strlen(str) + 1, 0);
+
+		// 接收客户端的数据
+		char recvBuffer[1024] = { 0 };
+		recv(clientSock, recvBuffer, 1024, 0);
+
+		std::cout << "recive message: " << recvBuffer << std::endl;
+
+		// 关闭客户端这一次的套接字
+		closesocket(clientSock);
+
+
+	}
+	closesocket(serverSock);
+
+	WSACleanup();
+
+    std::cout << "Hello World!\n"; 
+}
+
+/*
+ * 服务端流程：
+ * 1. 初始化
+ * 2. 创建套接字
+ * 3. 设置地址信息等
+ * 4. bind 绑定
+ * 5. listen 监听
+ * 6. accept 接收客户端请求
+ * 7. recive 接收客户端数据
+ * 8. send 向客户端发送数据
+ * 9. 关闭此次客户端请求套接字
+ * 10. 关闭服务端套接字
+ * 11. 清理
+ */
